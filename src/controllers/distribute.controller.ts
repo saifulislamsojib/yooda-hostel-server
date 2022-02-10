b785964870e9e605cbb61012ebefb27c07028286
saifulislamsojib <saifulislam.sojib56@gmail.com>
2022-02-10 22:10:36 +0600

    commited

--- src/controllers/distribute.controller.ts
+++ src/controllers/distribute.controller.ts
@@ -1,9 +1,14 @@
+import endOfDay from 'date-fns/endOfDay';
+import startOfDay from 'date-fns/startOfDay';
 import { Request, Response } from 'express';
 import Distribute from '../models/distribute.model';
+import Student from '../models/student.model';
 
 export const getTodaysDistributions = async (req: Request, res: Response) => {
     try {
-        const distributeData = await Distribute.find({ date: new Date() });
+        const distributeData = await Distribute.find({
+            date: { $gte: startOfDay(new Date()), $lte: endOfDay(new Date()) },
+        });
         res.status(200).json({ data: distributeData });
     } catch (error) {
         res.status(500).json(error);
@@ -12,17 +17,23 @@ export const getTodaysDistributions = async (req: Request, res: Response) => {
 
 export const distribute = async (req: Request, res: Response) => {
     try {
-        const already = await Distribute.findOne({
-            studentId: req.body.studentId,
-            date: { $gte: new Date(req.body.date) },
-            shift: req.body.shift,
-        });
-        if (!already) {
-            const distributeData = new Distribute(req.body);
-            await distributeData.save();
-            res.status(200).json({ data: distributeData });
+        const { studentId, date, shift } = req.body;
+        const isStudent = await Student.findOne({ roll: studentId });
+        if (isStudent) {
+            const already = await Distribute.findOne({
+                studentId,
+                shift,
+                date: { $gte: startOfDay(new Date(date)), $lte: endOfDay(new Date(date)) },
+            });
+            if (!already) {
+                const distributeData = new Distribute(req.body);
+                await distributeData.save();
+                res.status(200).json({ data: distributeData });
+            } else {
+                res.status(200).json({ error: 'Already served!' });
+            }
         } else {
-            res.status(200).json({ error: 'Already served!' });
+            res.status(200).json({ error: 'No Student Found By The Roll!' });
         }
     } catch (error) {
         res.status(500).json(error);
